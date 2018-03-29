0dce361ccb04ed8a3fe0a7a1ee52eddcab556ac6
Cong Peng <cong.peng@appfolio.com>
2018-03-29 16:44:57 -0700

    cp - add a new image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,23 @@ class ImagesController < ApplicationController
   def show
     @image = Image.find(params[:id])
   end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to image_path(@image), notice: 'The image is successfully added.'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:url)
+  end
 end
